556ff90436a3978fabd14277721f544185456243
Shreyas S Hegde <hegdeshreyas1408@gmail.com>
2026-07-30 18:45:00 +0530

    Add: sum-of-two-integers - submission-1

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int sum = a ^ b, carry = a & b;
+        int tempsum = 0, tempcarry = 0;
+        carry*=2;
+        while(carry != 0){
+            tempsum = (sum ^ carry);
+            carry = (sum & carry)*2;
+
+            sum = tempsum;
+        }
+
+        return sum;
+    }
+};
